1950a0a4885946f5f24c2881690390d985a95d6c
Joey Elsisi <joeyelsisi@gmail.com>
2022-11-02 21:12:52 -0400

    sleep is dangerous

--- src/screens/ScreenIntro.js
+++ src/screens/ScreenIntro.js
@@ -8,22 +8,28 @@ const ScreenIntro = () => {
 
             {/* Example section 1 */}
             <Grid item>
-                <Typography variant="h5" >Example Section 1 </Typography>
+                <Typography variant="h4" > Why we sleep </Typography>
             </Grid>
 
             <Grid container direction="row" >
                 <Grid item xs={8}>
-                <Typography variant="body1" >more spacemore space more space more space more space more space more space more space more spacemore space more space more space more space more space more space more space more spacemore space more space more space more space more space more space more space more spacemore space more space more space more space more space more space more space more spacemore space more space more space more space more space more space more space  </Typography>
+                    <Typography variant="h5"> Sleep is dangerous </Typography>
+                <Typography variant="body1" > We spend a third of our life asleep. This is time that could be spent
+                    reproducing, gathering food or gaining resources. Instead, we lie dormant, vulnerable to predators.
+                    <br/>
+                     Every animal sleeps, and so do some plants. So what is its purpose?
+                    </Typography>
                 </Grid>
                 <Grid item xs={4}>
-                    <Typography variant="body1" >less spaceless spaceless spaceless spaceless spaceless spaceless spaceless spaceless spaceless spaceless spaceless spaceless spaceless spaceless spaceless spaceless spaceless space </Typography>
+                    <Typography variant="h5">  &nbsp;  </Typography>
+                    <Typography variant="body1" >You’d expect larger animals to need more sleep, but the inverse is generally true. The owl monkey sleeps 17 hours a day, while Elephants sleep only 2 hours a day.</Typography>
                 </Grid>
             </Grid>
 
 
             {/* Example section 2 */}
             <Grid item>
-                <Typography variant="h5" >Example Section 2 </Typography>
+                <Typography variant="h4" > Theories for why we sleep</Typography>
             </Grid>
 
             <Grid container direction="row" >
