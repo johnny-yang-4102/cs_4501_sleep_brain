aca54fc4a24a428df635de6c3a281c300d3ea73a
Johnny <jyang8501@gmail.com>
2022-11-04 01:10:03 -0400

    1. Install MUI-image, add in portion of brain info

--- src/screens/ScreenIntro.js
+++ src/screens/ScreenIntro.js
@@ -1,8 +1,18 @@
 import { Stack } from "@mui/system";
-import { Grid, Typography } from "@mui/material";
+import { Grid, Typography, ImageList, ImageListItem } from "@mui/material";
+
+import Image from "mui-image";
+
+//Need to import all of your pictures before using
+import picBrain from '../images/brain.png'
+import picGlial from '../images/glial.webp'
+import picNeuron from '../images/neuron-structure.jpeg'
 
 
 const ScreenIntro = () => {
+
+
+
     return (
         <Stack spacing={4} direction="column" justifyContent="center" alignItems="center" marginLeft={15} marginRight={15}>
 
@@ -14,10 +24,10 @@ const ScreenIntro = () => {
             <Grid container direction="row" >
                 <Grid item xs={8}>
                     <Typography variant="h5"> Sleep is dangerous </Typography>
-                <Typography variant="body1" > We spend a third of our life asleep. This is time that could be spent
-                    reproducing, gathering food or gaining resources. Instead, we lie dormant, vulnerable to predators.
-                    <br/>
-                     Every animal sleeps, and so do some plants. So what is its purpose?
+                    <Typography variant="body1" > We spend a third of our life asleep. This is time that could be spent
+                        reproducing, gathering food or gaining resources. Instead, we lie dormant, vulnerable to predators.
+                        <br />
+                        Every animal sleeps, and so do some plants. So what is its purpose?
                     </Typography>
                 </Grid>
                 <Grid item xs={4}>
@@ -34,19 +44,108 @@ const ScreenIntro = () => {
 
             <Grid container direction="row" >
                 <Grid item xs={4}>
-                <Typography variant="body1" >equal space equal space equal space equal space equal space equal space equal space equal space equal space  </Typography>
+                    <Typography variant="body1" >equal space equal space equal space equal space equal space equal space equal space equal space equal space  </Typography>
                 </Grid>
                 <Grid item xs={4}>
-                <Typography variant="body1" >equal space equal space equal space equal space equal space equal space equal space equal space equal space  </Typography>
+                    <Typography variant="body1" >equal space equal space equal space equal space equal space equal space equal space equal space equal space  </Typography>
                 </Grid>
                 <Grid item xs={4}>
-                <Typography variant="body1" >equal space equal space equal space equal space equal space equal space equal space equal space equal space  </Typography>
+                    <Typography variant="body1" >equal space equal space equal space equal space equal space equal space equal space equal space equal space  </Typography>
                 </Grid>
             </Grid>
 
             <Grid item>
-                <Typography variant="h5">Heading 3</Typography>
+                <Typography variant="h3" > The Brain</Typography>
             </Grid>
+
+            {/* Example section 3 */}
+
+            <Grid container direction="row" >
+
+
+                {/* Title */}
+                <Grid item xs={12}>
+                    <Typography variant="h4" > What is the Function of the Brain?</Typography>
+                </Grid>
+
+                <Grid container direction="column" xs={6} >
+                    <Grid item >
+                        <Typography variant="body1" >The brain is a massive parallel, distributed control system that attempts to maintain certain metabolic variables within acceptable bounds in the body while fulfilling a procreative mission. More concretely, brains maintain homeostasis, motivation, learning and memory, and sleep. The brain also needs to do this while responding to a variety of unpredictable circumstances internal and external to the body!
+                        </Typography>
+                    </Grid>
+
+
+                    <Grid item >
+                        <Typography variant="body1" >In the animal kingdom, different types of organisms have varying nervous systems to house their brain. Invertebrates tend to contain distributed systems (i.e octopi with 9 brains, jelly fishes with “nerve nets”) while vertebrates yield more centralized and specialized systems. Vertebrates all share a basic structure of a central nervous system (CNS) containing a brain and spinal cord connected to a peripheral nervous system (PNS) made up of peripheral sensory and motor nerves.
+                        </Typography>
+                    </Grid>
+                </Grid>
+
+
+                <Grid item xs={6} >
+                    <Image src={picBrain} height="100%" width="50%" />
+                </Grid>
+            </Grid>
+
+
+
+            <Grid container direction="row" >
+
+                {/* Title */}
+                <Grid item xs={12}>
+                    <Typography variant="h4" > The General Structure of the Brain</Typography>
+                </Grid>
+
+                <Grid item xs={6} >
+                    <ImageList sx={{ width: 500, height: 164 }} cols={2} rowHeight={164}>
+                        <ImageListItem>
+                            <img src={picGlial} />
+                        </ImageListItem>
+                        <ImageListItem>
+                            <img src={picNeuron} />
+                        </ImageListItem>
+
+
+
+                    </ImageList>
+                </Grid>
+
+                <Grid container direction="column" xs={6} >
+                    <Grid item >
+                        <Typography variant="body1" >Brains contain two broad classes of cells: neurons, information transmitters, across connections called synapses. There are also glial cells, non-neuronal cells which provide physical and metabolic support to neurons.
+                        </Typography>
+                    </Grid>
+
+
+                </Grid>
+
+            </Grid>
+
+            <Grid container direction="row" >
+
+                {/* Title */}
+                <Grid item xs={12}>
+                    <Typography variant="h4" > Varying Different Parameters of Brain</Typography>
+                </Grid>
+
+                <Grid>
+
+                </Grid>
+
+                <Grid item xs={6} >
+                <Typography variant="h5" > Total Weight of Brain</Typography>
+                </Grid>
+
+                <Grid item xs={6} >
+                <Typography variant="h5" > Structure Sizes within Brain</Typography>
+                </Grid>
+
+
+
+            </Grid>
+
+
+
 
         </Stack>
     );
