f907299ed8a4f80324b4e850bfe32b2a97925a0c
Johnny Yang <jyang8501@gmail.com>
2022-12-06 20:19:52 -0500

    Finish brain screen

--- src/screens/ScreenBrain.js
+++ src/screens/ScreenBrain.js
@@ -4,9 +4,13 @@ import { Grid, Typography, ImageList, ImageListItem } from "@mui/material";
 import Image from "mui-image";
 
 //Need to import all of your pictures before using
-import picBrain from '../images/brain.png'
 import picGlial from '../images/glial.webp'
 import picNeuron from '../images/neuron-structure.jpeg'
+
+import picDifferentProcessing from '../images/brain-screen-different-processing-systems.jpeg'
+import picCrocodileHumanCompare from '../images/brain-screen-croc-human-brains.png'
+import picElephantHumanCompare from '../images/brain-screen-elephant-human-brain-comparison.jpeg'
+import picHumanBrain from '../images/brain-screen-human-brain.webp'
 
 const ScreenBrain = () => {
 
@@ -35,8 +39,9 @@ const ScreenBrain = () => {
 
                 {/* figure for human brain */}
                 {/* 3d figure of brain would be cooler */}
-                <Grid item sm={12} md={6}>
-                    <Image src={picBrain} height="100%" width="100%" />
+                <Grid container direction="row" sm={12} md={6}>
+                    <Image src={picDifferentProcessing} height="100%" width="100%" />
+                    <Typography variant="body1" align="center">The diversity of different nervous systems' composition and structure  </Typography>
                 </Grid>
             </Grid>
 
@@ -46,14 +51,44 @@ const ScreenBrain = () => {
             {/* ............................... */}
             <Grid container direction="row" spacing={3}>
                 <Grid item xs={12}>
-                    <Typography variant="h4" align="center"> The General Structure of the Brain</Typography>
+                    <Typography variant="h4" align="center"> The General Structure of a Human Brain</Typography>
+                </Grid>
+
+                <Grid container direction="row">
+                    <Grid item xs={12}>
+                        <Typography variant="h5" style={padding} align="center">Cell Composition</Typography>
+                    </Grid>
+
+                    <Grid item xs={12}>
+                        <Typography variant="body1" style={padding}>Brains contain two broad classes of cells: neurons and synapses. Neurons act as electrical signal transmitters within the brain. Synapses connect neurons together to actually pass these electrical signals. There are also glial cells, non-neuronal cells which provide physical and metabolic support to neurons. </Typography>
+                    </Grid>
+
+
+
+                    {/* Picture 1 */}
+                    <Grid container direction="row" xs={6}>
+                        <Grid item xs={12}>
+                            <Image src={picGlial} height="100%" width="100%" fit="fill" />
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <Typography variant="body1" align="center">Glial cells' structure</Typography>
+                        </Grid>
+                    </Grid>
+
+                    <Grid container direction="row" xs={6}>
+                        <Grid item xs={12}>
+                            <Image src={picNeuron} height="100%" width="100%" fit="fill" />
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <Typography variant="body1" align="center">Neuron structure</Typography>
+                        </Grid>
+                    </Grid>
                 </Grid>
 
 
                 <Grid container direction="column" xs={6}>
-                    <Typography variant="h5" style={padding}>Cell Composition</Typography>
-
-                    <Typography variant="body1" style={padding}>Brains contain two broad classes of cells: neurons and synapses. Neurons act as electrical signal transmitters within the brain. Synapses connect neurons together to actually pass these electrical signals. There are also glial cells, non-neuronal cells which provide physical and metabolic support to neurons. </Typography>
 
                     <Typography variant="h5" style={padding}>Basal Ganglia</Typography>
 
@@ -76,7 +111,24 @@ const ScreenBrain = () => {
                     <Typography variant="body1" style={padding}>Helps to realty signals between brain and spinal cord. Coordinates the motor control signals sent from the brain to the body. Also controls several important functions related to homeostasis such as breathing, arousal, and digestion.</Typography>
                 </Grid>
 
-                <Typography variant="h5" style={padding}>inserrt image here </Typography>
+                <Grid container direction="row" xs={6} >
+
+
+                    {/* <Grid container direction="column" xs={12}>
+                        <Image src={picNeuron} height="100%" width="100%" fit="fill" />
+                        <Typography variant="body1" align="center">neurons</Typography>
+                    </Grid> */}
+
+                    <Grid container direction="column" xs={12} alignSelf="center">
+                        <Image src={picHumanBrain} height="50%" width="100%" fit="fill" />
+                        <Typography variant="body1" align="center">Model of the human brain.</Typography>
+                    </Grid>
+
+                    {/* alligator v.s human v.s other brains */}
+                </Grid>
+                <Grid item xs={6}>
+                    <Typography variant="h5" style={padding}></Typography>
+                </Grid>
 
 
 
@@ -91,12 +143,22 @@ const ScreenBrain = () => {
                     <Typography variant="h4" align="center" > Varying Different Parameters of Brain</Typography>
                 </Grid>
 
-                <Grid container direction="column" xs={6}>
+                <Grid container direction="column">
                     <Typography variant="body1" style={padding}>The amount of ways that organisms' brains can vary is vast. Generally, there are trends for brain structures for categories of organisms. For instance, on average, predators have larger brains than their prey. Mammals have larger brain-to-body ratios than other vertebrates. To put this into perspective, an entire alligator's brain would fill up about one and a half teaspoons while a human brain weighs on average 1.5 kg.</Typography>
 
                     <Typography variant="body1" style={padding}>Generally brain structures that represent a greater percentage of volume also have higher relative importance to an animal's use. For instance, the olfactory bulb, a mass of tissue in the brain involved in the sense of smell, in a mouse versus a human brain is 2% to 0.01% of the brain by volume.</Typography>
 
-                    <Typography variant="body1" style={padding}>A larger brain or brain structures however does not indicate "more intelligence". Although the elephant cerebral cortex weighs twice as much as the human cortex, it only has 1/3 the amount of neurons humans have in their cerebral cortex the olfactory bulb, a mass of tissue in the brain involved in the sense of smell, in a mouse versus a human brain is 2% to 0.01% of the brain by volume.</Typography>
+                    <Typography variant="body1" style={padding}>A larger brain or brain structures however does not indicate "more intelligence". Although the elephant cerebral cortex weighs twice as much as the human cortex, it only has one-third the amount of neurons humans have in their cerebral cortex the olfactory bulb, a mass of tissue in the brain involved in the sense of smell, in a mouse versus a human brain is 2% to 0.01% of the brain by volume.</Typography>
+                </Grid>
+
+                <Grid container direction="row" justifyContent="center">
+
+                    <Grid container direction="row" xs={11}>
+                        <Image src={picElephantHumanCompare} height="100%" width="100%" />
+                        <Typography variant="body1" align="center">Info-sheet demonstrating human brain size relative to other animals. Note that although the elephant has a brain more than twice the size of a human's, it still contains three times less neurons.</Typography>
+                    </Grid>
+
+                    {/* alligator v.s human v.s other brains */}
                 </Grid>
 
 
